fbed6c08fcedf3b779e48e035ca8e03c38f27cbd
Niyoko Yuliawan <niyoko.yuliawan@stockbit.com>
2023-12-01 16:50:40 +0700

    add payload

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -2,23 +2,47 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/niyoko/family-assistant/src/infra"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+func respondWithError(status int, message string) ([]byte, error) {
+	body, err := json.Marshal(&struct {
+		Message string `json:"message"`
+	}{Message: message})
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshall body: %w", err)
+	}
+
+	resp := &infra.Response{
+		StatusCode:      status,
+		Body:            string(body),
+		IsBase64Encoded: false,
+	}
+
+	respJson, err := json.Marshal(resp)
+	if err != nil {
+		return nil, fmt.Errorf("failed to marshall response: %w", err)
+	}
+
+	return respJson, nil
 }
 
-func HandleRequest(ctx context.Context, event *MyEvent) (*string, error) {
-	if event == nil {
-		return nil, fmt.Errorf("received nil event")
+func HandleRaw(ctx context.Context, payload []byte) ([]byte, error) {
+	var decodedPayload *infra.GatewayEvent
+	err := json.Unmarshal(payload, &decodedPayload)
+	if err != nil {
+		return nil, fmt.Errorf("failed to unmarshal payload: %w", err)
 	}
-	message := fmt.Sprintf("Hello %s!", event.Name)
-	return &message, nil
+
+	fmt.Println(string(payload))
+	return nil, nil
 }
 
 func main() {
-	lambda.Start(HandleRequest)
+	lambda.Start(infra.LambdaHandler(HandleRaw))
 }
---
+++ src/infra/main.go
@@ -0,0 +1,44 @@
+package infra
+
+import "context"
+
+type GatewayEvent struct {
+	Version         string            `json:"version"`
+	RouteKey        string            `json:"routeKey"`
+	RawPath         string            `json:"rawPath"`
+	RawQuery        string            `json:"rawQueryString"`
+	PathParameters  map[string]string `json:"pathParameters"`
+	Headers         map[string]string `json:"headers"`
+	QueryParameters map[string]string `json:"queryStringParameters"`
+	RequestContext  struct {
+		ApiId string `json:"apiId"`
+		Http  struct {
+			Method    string `json:"method"`
+			Path      string `json:"path"`
+			Protocol  string `json:"protocol"`
+			SourceIp  string `json:"sourceIp"`
+			UserAgent string `json:"userAgent"`
+		} `json:"http"`
+	} `json:"requestContext"`
+	Body string `json:"body"`
+}
+
+type Response struct {
+	Cookies         []string          `json:"cookies"`
+	IsBase64Encoded bool              `json:"isBase64Encoded"`
+	StatusCode      int               `json:"statusCode"`
+	Headers         map[string]string `json:"headers"`
+	Body            string            `json:"body"`
+}
+
+type RouteHandler func(ctx context.Context, payload *GatewayEvent) (interface{}, error)
+
+func (handler RouteHandler) Invoke(ctx context.Context, payload *GatewayEvent) (interface{}, error) {
+	return handler(ctx, payload)
+}
+
+type LambdaHandler func(ctx context.Context, payload []byte) ([]byte, error)
+
+func (handler LambdaHandler) Invoke(ctx context.Context, payload []byte) ([]byte, error) {
+	return handler(ctx, payload)
+}
